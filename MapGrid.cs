53df502c5e7e3ef8b6ad50fe124981e062aef02c
naumenkoff <naumenkoff1337@gmail.com>
2022-10-08 14:17:14 +0300

    Added MapGrid.cs

---
+++ MapGrid.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+
+namespace Oxide.Plugins
+{
+    [Info("MapGrid", "naumenkoff", "0.1.0")]
+    internal class MapGrid : RustPlugin
+    {
+        private void Init()
+        {
+            cmd.AddConsoleCommand("mg.show", this, "GetGrid");
+        }
+
+        private void GetGrid(ConsoleSystem.Arg args)
+        {
+            if (args.IsAdmin == false) return;
+            
+            if (args.IsRcon)
+            {
+                if (args.HasArgs(3) == false)
+                {
+                    args.ReplyWith("Enter \"mg.show x y z\".");
+                    return;
+                }
+                var rconPosition = new Vector3(args.GetFloat(0), args.GetFloat(1), args.GetFloat(2));
+                args.ReplyWith($"{rconPosition.ToString()} -> [{PositionToGridCoord(rconPosition)}]");
+                return;
+            }
+            
+            var player = args.Player();
+            var position = player.transform.position;
+            player.ConsoleMessage($"{position.ToString()} -> [{PositionToGridCoord(position)}]");
+        }
+
+        // Token: 0x06002702 RID: 9986 RVA: 0x000D9D2C File Offset: 0x000D7F2C
+        // string PhoneController.PositionToGridCoord(vector3 position)
+        private string PositionToGridCoord(Vector3 position)
+        {
+            var a = new Vector2(TerrainMeta.NormalizeX(position.x), TerrainMeta.NormalizeZ(position.z));
+            var num = TerrainMeta.Size.x / 1024f;
+            const int num2 = 7;
+            var vector = a * num * num2;
+            var num3 = Mathf.Floor(vector.x) + 1f;
+            var num4 = Mathf.Floor((float)Math.Round(num) * num2 - vector.y) - 1;
+            var text = string.Empty;
+            var num5 = num3 / 26f;
+            var num6 = num3 % 26f;
+            if (num6 == 0f) num6 = 26f;
+            if (num5 > 1f) text += Convert.ToChar(64 + (int)num5).ToString();
+            text += Convert.ToChar(64 + (int)num6).ToString();
+            return $"{text}{num4}";
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# MapGrid
- A plugin for Rust that allows you to accurately determine the text-square of your location.
+# Map Grid
+
+Map Grid allows you to find out the text-square location based on the coordinates.
+
+## Use for developers (as __API__)
+
+__```string PositionToGridCoord(Vector3 position)```__
+
+Add my plugin as a reference to your plugin
+```
+[PluginReference] private Plugin MapGrid;
+```
+
+Next, call the __PositionToGridCoord__ method with a Vector3 type parameter and cast to string
+```
+(string) MapGrid.Call("PositionToGridCoord", Vector3.zero)
+```
+
+__Example:__
+```
+[PluginReference] private Plugin MapGrid;
+
+[ChatCommand("myPosition")]
+private void DisplayPlayerGrid(BasePlayer player)
+{
+    var gridPosition = (string) MapGrid.Call("PositionToGridCoord", player.transform.position);
+    player.ChatMessage(gridPosition);
+}
+```
+
+## Using the plugin itself
+It can be used both through the in-game console and through the server console.
+##### Via Server Console
+```mg.show x y z```
+__Example:__
+```
+> mg.show 322.2 533.3 144.4
+(322.2, 533.3, 144.4) -> [J5]
+```
+
+##### Via In-Game Console
+```mg.show```
+__Example:__
+```
+> mg.show
+(60.0, 36.0, 255.1) -> [H4]
+```
+
